e00b892a955197468eb2fabb46aa958447a6e5b2
Calvin Loveland <calvin@loveland.dev>
2023-10-08 11:07:44 -0600

    update task class code and text workspace

--- src/operationalize/tasks/task.py
+++ src/operationalize/tasks/task.py
@@ -1,13 +1,18 @@
-
-
-class Task:
-    def __init__(self, name, broad_description, requirements, deliverable_description, time_limit, type, workspace):
-        self.name = name
-        self.broad_description = broad_description
-        self.requirements = requirements
-        self.deliverable_description = deliverable_description
-        self.time_limit = time_limit
-        self.type = type
-        self.workspace = workspace
-
-
+class Task_DAG:
+    def __init__(self, depedencies, dependents, **kwargs):
+        self.depedencies = depedencies
+        self.dependents = dependents
+        expected_kwargs = [
+            "name",
+            "type",
+            "description",
+            "requirements",
+            "workspace",
+            "time_limit",
+            "output",
+        ]
+        for key in kwargs:
+            if key not in expected_kwargs:
+                raise ValueError(f"Unexpected keyword argument: {key}")
+        for key, value in kwargs.items():
+            setattr(self, key, value)
