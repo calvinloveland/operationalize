2f500ae72cb60be1623c93df1b3e106e16c33835
Calvin Loveland <calvin@loveland.dev>
2023-11-04 18:59:20 +0000

    add tests and devcontainer

---
+++ src/operationalize/tasks/test/test_task.py
@@ -0,0 +1,92 @@
+from operationalize.tasks.task import TaskDAG
+from copy import deepcopy
+from loguru import logger
+import sys
+
+# Set logging level stdout to DEBUG
+logger.remove()
+logger.add(sys.stdout, level="DEBUG")
+
+
+def test_task_dag():
+    task_dag = TaskDAG()
+    assert task_dag.name == "TASK WITH NO NAME"
+    assert task_dag.get_next_task() == task_dag
+
+
+def test_task_deep_copy():
+    task_dag = TaskDAG()
+    new_task_dag = deepcopy(task_dag)
+    assert new_task_dag.name == task_dag.name
+    assert new_task_dag.id != task_dag.id
+
+
+def test_task_dag_to_mermaid_chart():
+    task_dag = TaskDAG()
+    task_dag.to_mermaid_flowchart()
+    assert True
+
+
+def test_task_dag_to_mermaid_chart_with_dependents():
+    task_dag = TaskDAG()
+    task_dag.append_node(TaskDAG())
+    task_dag.to_mermaid_flowchart()
+    assert True
+
+
+def test_task_dag_update_dependencies():
+    task_dag = TaskDAG()
+    task_dag.append_node(TaskDAG())
+    task_dag.update_dependencies()
+    assert True
+
+
+def test_task_dag_get_expected_completion_time():
+    task_dag = TaskDAG()
+    task_dag.append_node(TaskDAG())
+    task_dag.update_dependencies()
+    assert task_dag.get_expected_completion_time() == 240
+
+
+def test_task_dag_get_final():
+    task_dag = TaskDAG()
+    another_task_dag = TaskDAG()
+    task_dag.append_node(another_task_dag)
+    task_dag.get_final() == another_task_dag
+
+
+def test_task_dag_get_open_tasks():
+    task_dag = TaskDAG()
+    task_dag.append_node(TaskDAG())
+    task_dag.update_dependencies()
+    assert len(task_dag.get_open_tasks()) == 1
+
+
+def test_task_dag_count_open_tasks():
+    task_dag = TaskDAG()
+    task_dag.append_node(TaskDAG())
+    task_dag.update_dependencies()
+    assert task_dag.count_open_tasks() == 1
+
+
+def test_task_dag_assign():
+    task_dag = TaskDAG()
+    task_dag.assign("worker_id")
+    assert task_dag.assigned_to == "worker_id"
+
+
+def test_task_dag_task_is_ready():
+    task_dag = TaskDAG()
+    another_task_dag = TaskDAG()
+    task_dag.append_node(another_task_dag)
+    task_dag.update_dependencies()
+    assert task_dag.task_is_ready() == True
+    assert another_task_dag.task_is_ready() == False
+
+
+def test_task_dag_get_task_by_id():
+    task_dag = TaskDAG()
+    another_task_dag = TaskDAG()
+    task_dag.append_node(another_task_dag)
+    task_dag.update_dependencies()
+    assert task_dag.get_task_by_id(another_task_dag.id) == another_task_dag
