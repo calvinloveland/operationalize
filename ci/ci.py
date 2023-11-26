901ad1fb0cf917da754c24eca7117fd001c858f4
Calvin Loveland <calvin@loveland.dev>
2023-11-26 21:53:12 +0000

    add more ci checks

--- ci/ci.py
+++ ci/ci.py
@@ -7,3 +7,15 @@ result = subprocess.run(["pytest", "-v"])
 if result.returncode != 0:
     print("Tests failed!")
     exit(1)
+    
+# Run prospector
+result = subprocess.run(["prospector"])
+if result.returncode != 0:
+    print("Prospector found issues!")
+    exit(1)
+
+# Run black
+result = subprocess.run(["black", "--check", "."])
+if result.returncode != 0:
+    print("Black found formatting issues!")
+    exit(1)
